181be180b5a1ff07795033a8a99bccb5de3753bb
Fernando Bevilacqua <dovyski@gmail.com>
2021-06-17 17:44:06 -0300

    feat: initial structure

---
+++ src/PollFromText.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CCUFFS\Text;
+
+/**
+ * 
+ *
+ * @author Fernando Bevilacqua <fernando.bevilacqua@uffs.edu.br>
+ */
+class PollFromText
+{
+    /**
+     * 
+     */
+    public function parse($text)
+    {
+        if(empty($text)) {
+            return [];
+        }
+
+        return [];
+    }
+
+}
---
+++ tests/Unit/InstanceTest.php
@@ -0,0 +1,6 @@
+<?php
+
+it('tests construtor with no params', function() {
+    $poller = new CCUFFS\Text\PollFromText();
+    expect($poller)->toBeInstanceOf(CCUFFS\Text\PollFromText::class);
+});
